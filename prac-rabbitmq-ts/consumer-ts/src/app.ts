9b70345aceafa892d240e0335023ee5b27797364
ArchaeG <archepro84@gmail.com>
2022-10-24 23:53:03 +0900

    refactor: Refactoring project structure for consumer-ts project app.js

--- prac-rabbitmq-ts/consumer-ts/src/app.ts
+++ prac-rabbitmq-ts/consumer-ts/src/app.ts
@@ -1,37 +1,12 @@
-import * as client from 'amqplib';
-import { Channel, Connection, ConsumeMessage } from 'amqplib';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-const filePath = process.env.NODE_ENV === 'prod' ? '.prod.env' : '.dev.env';
-dotenv.config({ path: path.resolve(`${__dirname}../../../${filePath}`) });
-
-const rabbitMqConfigs = {
-  host: process.env.RABBIT_MQ_HOST,
-  user: process.env.RABBIT_MQ_USER,
-  password: process.env.RABBIT_MQ_PASSWORD,
-  port: process.env.RABBIT_MQ_PORT,
-  url: process.env.RABBIT_MQ_URL,
-};
+import { RabbitMQConsumerClient } from './modules/rabbitmq-consumer.client';
+import { ConsumeMessage } from 'amqplib';
 
 async function main(): Promise<void> {
-  const connection: Connection = await client.connect(
-    rabbitMqConfigs.url ? rabbitMqConfigs.url :
-      `amqps://${rabbitMqConfigs.user}:${rabbitMqConfigs.password}@${rabbitMqConfigs.host}/${rabbitMqConfigs.user}`,
-  );
+  const rabbitMQConsumerClient: RabbitMQConsumerClient = new RabbitMQConsumerClient();
 
-  const queueName = 'arch-queue';
+  await rabbitMQConsumerClient.connect();
 
-  // Rabbit MQ의 Channel을 생성합니다.
-  const channel: Channel = await connection.createChannel();
-
-  // Client가 Queue를 사용할 수 있도록 생성합니다.
-  await channel.assertQueue(queueName);
-
-
-  // queueName에 해당하는 Queue의 데이터를 수신합니다.
-  //  데이터를 수신할 때, onMessageConsumeEvent에 해당하는 CallBack 이벤트가 실행됩니다.
-  await channel.consume(queueName, onMessageConsumeEvent);
+  rabbitMQConsumerClient.consume(onMessageConsumeEvent);
 
 }
 
@@ -40,6 +15,5 @@ function onMessageConsumeEvent(_message: ConsumeMessage | null) {
     return console.log(_message.content.toString());
   }
 }
-
 
 main();
