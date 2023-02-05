c4cddee9313454460d3a589f326f0c93048185d2
Cobb Lee <archepro84@gmail.com>
2023-02-05 23:25:28 +0900

    feat: Remove app service layer by server-hero and server-company

--- prac-nest-grpc/server-hero/src/app.controller.ts
+++ prac-nest-grpc/server-hero/src/app.controller.ts
@@ -1,20 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { GrpcMethod } from "@nestjs/microservices";
-import { HeroById, HeroesService } from '../../proto/build/hero';
+import { Hero, HeroById, HeroesService } from '../../proto/build/hero';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {
-  }
+  private readonly heroArray: Hero[] = [
+    {
+      id: 1,
+      firstName: "foo1",
+      lastName: "bar1"
+    },
+    {
+      id: 2,
+      firstName: 'foo2',
+      lastName: 'bar2'
+    }
+  ];
 
   @GrpcMethod('HeroesService', 'FindOne')
   findOne(data: HeroById) {
-    return this.appService.findOne(data);
+    return this.heroArray.find(({ id }) => id === data.id);
   }
 
   @GrpcMethod('HeroesService', 'FindAll')
   findAll() {
-    return this.appService.findAll();
+    return this.heroArray;
   }
 }
