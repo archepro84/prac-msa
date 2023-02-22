352a6ea37f9ba2635e34500e321c3ac0809b8b6e
Cobb Lee <archepro84@gmail.com>
2023-02-22 12:05:09 +0900

    feat: Add company, hero client module template

--- prac-nest-grpc/client/src/app.module.ts
+++ prac-nest-grpc/client/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppGrpcService } from './app.grpc.service';
+import { HeroModule } from './hero/hero.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [],
+  imports: [HeroModule, CompanyModule],
   controllers: [AppController],
   providers: [AppGrpcService],
 })
