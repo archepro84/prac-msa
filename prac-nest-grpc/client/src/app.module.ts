7a38c9d15a9e6982d2783df858fbbeef031140b2
Cobb Lee <archepro84@gmail.com>
2023-02-22 12:17:21 +0900

    feat: Remove app domain controlelr and service

--- prac-nest-grpc/client/src/app.module.ts
+++ prac-nest-grpc/client/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppGrpcService } from './app.grpc.service';
 import { HeroModule } from './hero/hero.module';
 import { CompanyModule } from './company/company.module';
 
 @Module({
   imports: [HeroModule, CompanyModule],
-  controllers: [AppController],
-  providers: [AppGrpcService],
 })
 export class AppModule {}
